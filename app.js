b51399ab00f33eb9e175a500f3a9b29640920536
Abdifatah.206 <shire2567@gmail.com>
2019-09-05 23:45:34 -0700

    file room renamed

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const chatRouter = require('./routes/chat');
+
+
+const app = express();
+server = app.listen(3000)
+const io = require('socket.io')(server)
+io.on('connection', (socket) => {
+    console.log('new user connected')
+    
+    socket.username = "Anonymous"
+    
+    socket.on("change username", (data) => {
+        socket.username = data.username
+    })
+    socket.on('new messge', (data) => {
+        io.socket.emit('new_message', {message : data.message, username : socket.username});
+        
+    })
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', {username : socket.username})
+    })
+})
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +45,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/chat', chatRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
